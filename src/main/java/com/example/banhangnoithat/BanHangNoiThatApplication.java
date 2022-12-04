42c6377a7472aae446ab4a5be9b6c80b3e589def
nguyenmanhhung <nguyenmanhhung23022001@gmail.com>
2022-12-04 09:21:44 +0700

    create new  project

---
+++ src/main/java/com/example/banhangnoithat/BanHangNoiThatApplication.java
@@ -0,0 +1,13 @@
+package com.example.banhangnoithat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanHangNoiThatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanHangNoiThatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/banhangnoithat/BanHangNoiThatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banhangnoithat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanHangNoiThatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
